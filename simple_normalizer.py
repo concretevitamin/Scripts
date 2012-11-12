6f412d8b6fce30dfdd65133128e47cdc5f755b73
Zongheng Yang <concretevitamin@gmail.com>
2012-11-12 11:43:46 -0800

    simple_normalizer

---
+++ simple_normalizer.py
@@ -0,0 +1,15 @@
+import sys
+
+def simple_normalize(paths):
+    """Given a list of paths, print normalized paths to stdout one per line."""
+    for path in paths:
+        splits = path.strip().split('/')
+        if splits[-1] == '.': splits.append('')
+        take = [False] * len(splits)
+        for i in xrange(0, len(splits)):
+            if splits[i] == '..' and i > 0: take[i - 1] = False
+            if splits[i] not in ['.', '..']: take[i] = True
+        print '/'.join([tup[0] for tup in zip(splits, take) if tup[1]])
+
+if __name__ == '__main__':
+    simple_normalize(sys.stdin.readlines())
