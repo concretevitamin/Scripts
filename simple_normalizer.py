1885bad46d5b9187723fd124b71364da148534df
Zongheng Yang <concretevitamin@gmail.com>
2012-11-12 17:43:41 -0800

    purify simple_normalize()

--- simple_normalizer.py
+++ simple_normalizer.py
@@ -1,11 +1,12 @@
 """Usage: python simple_normalize
 The script reads multiple lines from stdin, treating each line as a path to
-normalize.
+normalize, and prints normalized paths to stdout one per line.
 """
 import sys
 
 def simple_normalize(paths):
-    """Given a list of paths, print normalized paths to stdout one per line."""
+    """Given a list of paths, return a list of normalized paths."""
+    res = []
     for path in paths:
         splits = path.strip().split('/')
         if splits[-1] == '.': splits.append('')
@@ -13,7 +14,8 @@ def simple_normalize(paths):
         for i in xrange(0, len(splits)):
             if splits[i] == '..' and i > 0: take[i - 1] = False
             if splits[i] not in ['.', '..']: take[i] = True
-        print '/'.join([tup[0] for tup in zip(splits, take) if tup[1]])
+        res.append('/'.join([tup[0] for tup in zip(splits, take) if tup[1]]))
+    return res
 
 if __name__ == '__main__':
-    simple_normalize(sys.stdin.readlines())
+    print '\n'.join(simple_normalize(sys.stdin.readlines()))
